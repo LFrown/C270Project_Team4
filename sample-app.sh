c4b6a0a2d22b91299bf85289d658bc9d59a20248
23048475-JiaEn <23048475@myrp.edu.sg>
2025-01-19 22:15:43 +0800

    Create sample-app.sh

---
+++ sample-app.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Step 1: Build the Docker image
+docker build -t inventory-manager .
+
+# Step 2: Run tests inside the Docker container
+docker run --rm inventory-manager python3 -m pytest tests/
+
+# Step 3: Run the application in the background
+docker run -d --name inventory-manager-app -p 5000:5000 inventory-manager
+
+echo "Application is running at: http://localhost:5000"
